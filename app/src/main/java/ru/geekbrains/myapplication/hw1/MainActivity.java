90d6a836a00edd2f3b59d5c54fc9f9c5bfb190b6
Aleksandra <lionessa@km.ru>
2022-01-24 04:17:14 +1000

    hw1

---
+++ app/src/main/java/ru/geekbrains/myapplication/hw1/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.myapplication.hw1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.geekbrains.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
--- app/src/main/java/ru/geekbrains/myapplication/hw2/MainActivity.java
+++ app/src/main/java/ru/geekbrains/myapplication/hw2/MainActivity.java
@@ -1,14 +1,17 @@
-package ru.geekbrains.myapplication;
+package ru.geekbrains.myapplication.hw2;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import ru.geekbrains.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
+
 }
