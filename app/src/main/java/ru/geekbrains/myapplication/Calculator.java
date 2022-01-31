36beab2029789f79dbafd31876bf34e4278411eb
Aleksandra <lionessa@km.ru>
2022-02-01 00:29:06 +1000

    lesson3

---
+++ app/src/main/java/ru/geekbrains/myapplication/Calculator.java
@@ -0,0 +1,137 @@
+package ru.geekbrains.myapplication;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Calculator implements Parcelable {
+
+    private StringBuilder stringBuilder;
+    private boolean result;
+    private boolean operation;
+    private Context mainActivity;
+    private int counterDotSecond;
+
+    public Calculator(boolean operation, boolean result, int counterDotSecond, Context mainActivity) {
+        if (!result) {
+            stringBuilder = new StringBuilder();
+            this.result = false;
+            this.operation = operation;
+            this.mainActivity = mainActivity;
+            this.counterDotSecond = counterDotSecond;
+        }
+    }
+
+    protected Calculator(Parcel in) {
+        stringBuilder.append(in.readString());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            result = in.readBoolean();
+            operation = in.readBoolean();
+        }
+        counterDotSecond = in.readInt();
+    }
+
+    public static final Creator<Calculator> CREATOR = new Creator<Calculator>() {
+        @Override
+        public Calculator createFromParcel(Parcel in) {
+            return new Calculator(in);
+        }
+
+        @Override
+        public Calculator[] newArray(int size) {
+            return new Calculator[size];
+        }
+    };
+
+    public int getCounterDotSecond() {
+        return counterDotSecond;
+    }
+
+    public void setCounterDotSecond(int counterDotSecond) {
+        this.counterDotSecond = counterDotSecond;
+    }
+
+    public void increaseCounterDotSecond() {
+        counterDotSecond++;
+    }
+
+    public boolean isOperation() {
+        return !operation;
+    }
+
+    public void setOperation(boolean operation) {
+        this.operation = operation;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(Boolean result) {
+        this.result = result;
+    }
+
+    public StringBuilder getStringBuilder() {
+        return stringBuilder;
+    }
+
+    public void setStringBuilder(String string) {
+        stringBuilder.append(string);
+    }
+
+    public Double resultStringBuilder() {
+        String s = this.getStringBuilder().toString();
+        String[][] strings = new String[4][];
+        strings[0] = s.split(mainActivity.getString(R.string.split_plus));
+        strings[1] = s.split(mainActivity.getString(R.string.split_minus));
+        strings[2] = s.split(mainActivity.getString(R.string.split_multiply));
+        strings[3] = s.split(mainActivity.getString(R.string.split_divide));
+        int count = 0;
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].length > 1) {
+                count = i;
+            }
+        }
+        double res = 0;
+        switch (count) {
+            case 0: {
+                res = Double.parseDouble(String.valueOf(strings[count][0])) +
+                        Double.parseDouble(String.valueOf(strings[count][1]));
+                break;
+            }
+            case 1: {
+                res = Double.parseDouble(String.valueOf(strings[count][0])) -
+                        Double.parseDouble(String.valueOf(strings[count][1]));
+                break;
+            }
+            case 2: {
+                res = Double.parseDouble(String.valueOf(strings[count][0])) *
+                        Double.parseDouble(String.valueOf(strings[count][1]));
+                break;
+            }
+            case 3: {
+                res = Double.parseDouble(String.valueOf(strings[count][0])) /
+                        Double.parseDouble(String.valueOf(strings[count][1]));
+                break;
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(String.valueOf(stringBuilder));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            parcel.writeBoolean(result);
+            parcel.writeBoolean(operation);
+        }
+        parcel.writeInt(counterDotSecond);
+    }
+}
+
--- app/src/main/java/ru/geekbrains/myapplication/MainActivity.java
+++ app/src/main/java/ru/geekbrains/myapplication/MainActivity.java
@@ -1,14 +1,187 @@
 package ru.geekbrains.myapplication;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.HashSet;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final HashSet<Button> buttons = new HashSet<>();
+    private TextView textViewResult;
+    private Calculator calculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        for (Button button : buttons) {
+            button.setOnClickListener(listener);
+        }
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("calculator", calculator);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        calculator = savedInstanceState.getParcelable("calculator");
+        textViewResult.setText(calculator.getStringBuilder());
+    }
+
+    private void init() {
+        buttons.add(findViewById(R.id.button0));
+        buttons.add(findViewById(R.id.button1));
+        buttons.add(findViewById(R.id.button2));
+        buttons.add(findViewById(R.id.button3));
+        buttons.add(findViewById(R.id.button4));
+        buttons.add(findViewById(R.id.button5));
+        buttons.add(findViewById(R.id.button6));
+        buttons.add(findViewById(R.id.button7));
+        buttons.add(findViewById(R.id.button8));
+        buttons.add(findViewById(R.id.button9));
+        buttons.add(findViewById(R.id.button_dot));
+        buttons.add(findViewById(R.id.button_plus));
+        buttons.add(findViewById(R.id.button_minus));
+        buttons.add(findViewById(R.id.button_multiply));
+        buttons.add(findViewById(R.id.button_divide));
+        buttons.add(findViewById(R.id.button_equal));
+        textViewResult = findViewById(R.id.textView_result);
+        calculator = new Calculator(false, false, 0, this);
+    }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (calculator.isResult()) {
+                calculator.getStringBuilder().delete(0, calculator.getStringBuilder().capacity());
+                calculator.setResult(false);
+                calculator.setOperation(false);
+                calculator.setCounterDotSecond(0);
+            }
+            switch (view.getId()) {
+                case (R.id.button0): {
+                    if (!calculator.getStringBuilder().toString().equals(getString(R.string.zero))) {
+                        calculator.setStringBuilder(getString(R.string.zero));
+                    }
+                    break;
+                }
+                case (R.id.button1): {
+                    calculator.setStringBuilder(getString(R.string.one));
+                    break;
+                }
+                case (R.id.button2): {
+                    calculator.setStringBuilder(getString(R.string.two));
+                    break;
+                }
+                case (R.id.button3): {
+                    calculator.setStringBuilder(getString(R.string.three));
+                    break;
+                }
+                case (R.id.button4): {
+                    calculator.setStringBuilder(getString(R.string.four));
+                    break;
+                }
+                case (R.id.button5): {
+                    calculator.setStringBuilder(getString(R.string.five));
+                    break;
+                }
+                case (R.id.button6): {
+                    calculator.setStringBuilder(getString(R.string.six));
+                    break;
+                }
+                case (R.id.button7): {
+                    calculator.setStringBuilder(getString(R.string.seven));
+                    break;
+                }
+                case (R.id.button8): {
+                    calculator.setStringBuilder(getString(R.string.eight));
+                    break;
+                }
+                case (R.id.button9): {
+                    calculator.setStringBuilder(getString(R.string.nine));
+                    break;
+                }
+                case (R.id.button_dot): {
+                    String a = calculator.getStringBuilder().toString();
+
+                    boolean bool = false;
+                    boolean boolOperation = false;
+
+                    if (a.contains(getString(R.string.dot))) {
+                        bool = true;
+                        if (a.contains(getString(R.string.plus))
+                                || a.contains(getString(R.string.minus))
+                                || a.contains(getString(R.string.multiply))
+                                || a.contains(getString(R.string.divide))) {
+                            boolOperation = true;
+                        }
+                    }
+
+                    if (!bool) {
+                        calculator.setStringBuilder(getString(R.string.dot));
+                    } else if (boolOperation && calculator.getCounterDotSecond() == 0) {
+                        calculator.increaseCounterDotSecond();
+                        calculator.setStringBuilder(getString(R.string.dot));
+                    }
+                    break;
+                }
+                case (R.id.button_plus): {
+                    if (calculator.isOperation()) {
+                        calculator.setOperation(true);
+                        calculator.setStringBuilder(getString(R.string.plus));
+                    }
+                    break;
+                }
+                case (R.id.button_minus): {
+                    if (calculator.isOperation()) {
+                        calculator.setOperation(true);
+                        calculator.setStringBuilder(getString(R.string.minus));
+                    }
+                    break;
+                }
+                case (R.id.button_multiply): {
+                    if (calculator.isOperation()) {
+                        calculator.setOperation(true);
+                        calculator.setStringBuilder(getString(R.string.multiply));
+                    }
+                    break;
+                }
+                case (R.id.button_divide): {
+                    if (calculator.isOperation()) {
+                        calculator.setOperation(true);
+                        calculator.setStringBuilder(getString(R.string.divide));
+                    }
+                    break;
+                }
+                case (R.id.button_equal): {
+                    calculator.setResult(true);
+                    double doubleResult = calculator.resultStringBuilder();
+                    calculator.getStringBuilder().delete(0, calculator.getStringBuilder().capacity());
+                    int intResult = (int) Math.floor(doubleResult);
+                    double temp = doubleResult - intResult;
+                    if (temp > 0) {
+                        calculator.setStringBuilder(String.format(Locale.ROOT, "%.4f", doubleResult));
+                    } else {
+                        calculator.setStringBuilder(String.format(Locale.ROOT, "%d", intResult));
+                    }
+                    break;
+                }
+            }
+            textViewResult.setText(calculator.getStringBuilder());
+        }
+    };
+
 }
