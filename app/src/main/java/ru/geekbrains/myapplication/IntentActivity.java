22b5409459d46fb9eea18c39371a6c31bf3a5ffb
Aleksandra <lionessa@km.ru>
2022-02-08 22:34:05 +1000

    lesson5 intent отправляем в IntentActivity и принимаем в MainActivity, все указания в манефесте

---
+++ app/src/main/java/ru/geekbrains/myapplication/IntentActivity.java
@@ -0,0 +1,73 @@
+package ru.geekbrains.myapplication;
+
+import static ru.geekbrains.myapplication.MainActivity.INTENT_ACTIVITY_KEY;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class IntentActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+
+        setMyDrawable();
+
+        getIntentMyResult();
+    }
+
+    public void getIntentMyResult() {
+        textView = findViewById(R.id.textView_intent_activity);
+
+        Bundle bundle = getIntent().getExtras();
+        textView.setText(bundle.getString(INTENT_ACTIVITY_KEY));
+    }
+
+    public void setMyDrawable() {
+        ConstraintLayout constraintLayout = findViewById(R.id.intent_constraint_layout);
+        if (constraintLayout.getBackground() == null)
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                constraintLayout.setBackgroundResource(R.drawable.texture_wood_grain_weathered_washed_off_wooden_structure_grain_structure_background);
+            } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                constraintLayout.setBackgroundResource(R.drawable.heart_wood_love_wooden_old_heart_background_love_heart_romantic);
+            }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        setIntentToMain();
+    }
+
+    // Если брать ключи из ресурсов, то тоже работает,
+    // но вы нам ни разу не говорили о таком подходе.
+    // Интересно это не используется или есть какие-то подводные камни?
+
+    private void setIntentToMain() {
+        Intent intent = new Intent(getResources().getString(R.string.my_main));
+        if (!textView.getText().toString().equals(getResources().getString(R.string.empty))) {
+            intent.putExtra(getString(R.string.my_main), getResources().getString(R.string.my_main));
+        }
+//        В моем случае у главной активити, которая принимает интент, два интрент-фильтра,
+//        один с категорией лаунчер, а другой с категорией дефолт.
+//        Если у активити в манифесте только один интент-фильтр и этот фильтр с категорией лаунчер,
+//        то отправляя из другого приложения интент нужно прописать, проверку на наличие
+//        возможности обработки интента:
+
+//        @SuppressLint({"WrongConstant", "QueryPermissionsNeeded"})
+//        ActivityInfo activityInfo = intent.resolveActivityInfo(getPackageManager(), intent.getFlags());
+//        if (activityInfo != null) {
+        startActivity(intent);
+        finish();
+//        }
+    }
+}
--- app/src/main/java/ru/geekbrains/myapplication/MainActivity.java
+++ app/src/main/java/ru/geekbrains/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private Calculator calculator;
     public static int REQUEST_CODE = 999;
     public static String MAIN_KEY = "main_key";
+    public static String INTENT_ACTIVITY_KEY = "intent_activity";
 
     private static final String SP_NAME = "name_sp";
     private static final String SP_KEY = "name_sp";
@@ -34,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
 
         initView();
         initListeners();
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle == null) {
+            textViewResult.setText(getString(R.string.empty));
+            Log.e("BUNDLE", "NULL");
+        } else if (bundle.getString(getString(R.string.my_main)) != null) {
+            textViewResult.setText(bundle.getString(getString(R.string.my_main)));
+        }
     }
 
     @Override
@@ -67,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
         buttons.add(findViewById(R.id.button_divide));
         buttons.add(findViewById(R.id.button_equal));
         buttons.add(findViewById(R.id.button_choose_a_theme));
+        buttons.add(findViewById(R.id.button_to_intent_activity));
+
         textViewResult = findViewById(R.id.textView_result);
         calculator = new Calculator(false, false, 0, 0, this);
     }
@@ -150,6 +162,15 @@ public class MainActivity extends AppCompatActivity {
                     startActivityIfNeeded(intent, REQUEST_CODE);
                     break;
                 }
+                case (R.id.button_to_intent_activity): {
+                    Intent intent = new Intent(getString(R.string.my_result));
+                    if (calculator.getStringBuilder() != null) {
+                        intent.putExtra(INTENT_ACTIVITY_KEY, calculator.getStringBuilder().toString());
+                        calculator.setStringBuilder(getResources().getString(R.string.empty));
+                    }
+                    startActivity(intent);
+                    break;
+                }
             }
             textViewResult.setText(calculator.getStringBuilder());
         };
@@ -186,8 +207,9 @@ public class MainActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         LinearLayout linearLayout = findViewById(R.id.main_linear_layout);
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            linearLayout.setBackgroundResource(R.drawable.sunset_purple_sky_beach_sand_footprints_shore_water);
-        }
+        if (linearLayout.getBackground() == null)
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                linearLayout.setBackgroundResource(R.drawable.sunset_purple_sky_beach_sand_footprints_shore_water);
+            }
     }
 }
